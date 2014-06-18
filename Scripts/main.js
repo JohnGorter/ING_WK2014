0c7c7d4fe0df4797d861521977319234df8815da
JohnGorter <john.gorter@gmail.com>
2014-06-18 11:03:33 +0200

    Javascript

--- Scripts/main.js
+++ Scripts/main.js
@@ -3,8 +3,18 @@
     // Main
     $(function(){
        // $.ajax({ url:'http://ip.jsontest.com/', cache:false, success: function(data){ alert(JSON.stringify(data));}});
+        $("#date").val(datestring);
         
         $("#btnSave").click(function(){
+            if ($("#game").val() == "" || $("#game").val() == "vul in") {
+                $("#gamegroup").addClass("has-error");
+                $("span", "#gamegroup").addClass("glyphicon-remove");
+                return;
+            }else{
+                $("#gamegroup").removeClass("has-error");
+                $("span", "#gamegroup").removeClass("glyphicon-remove");
+            }
+                
             var li = $("<li class='list-group-item'>");
             var datespan = $("<span class='date'>").text($("#date").val() + " ");
             var titlespan = $("<span class='title'>").text($("#game").val() + " ");
@@ -14,6 +24,8 @@
                 });
             
             li.append(deletespan).append(datespan).append(titlespan).append(descspan).click(clickLI).appendTo($("#gamelist"));
+            
+            $("#game").val("");
         });
     });
     
