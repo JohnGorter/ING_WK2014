a52c41e999308759974b3c348faa1a87445eac2d
JohnGorter <john.gorter@gmail.com>
2014-06-20 11:45:02 +0200

    john

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,8 +1,18 @@
 
-    //jQuery.support.cors = true;
+   var games = new Array();
+   
+    jQuery.support.cors = true;
+    
     // Main
     $(function(){
-       // $.ajax({ url:'http://ip.jsontest.com/', cache:false, success: function(data){ alert(JSON.stringify(data));}});
+        if (window.localStorage['games']) {
+            games = JSON.parse(window.localStorage['games']);
+            for (game in games)
+                insertGame(games[game], false);
+        }
+        
+        //$.ajax({ url:'http://www.johngorter.com/score/getscores', cache:false, success: function(data){ alert(JSON.stringify(data));}});
+        
         var datestring = new Date().getFullYear() + "-0" + (new Date().getMonth() + 1) +"-" + new Date().getDate();
         $("#date").val(datestring);
         
@@ -13,11 +23,26 @@
                 
                 return ;
             }
-                
-            var li = $("<li class='list-group-item'>");
-            var datespan = $("<span class='date'>").text($("#date").val() + " ");
-            var titlespan = $("<span class='title'>").text($("#game").val() + " ");
-            var descspan = $("<span class='desc'>").text($("#description").val()).addClass("hidden");
+             var game = {
+               title: $("#game").val(),
+               description: $("#description").val(),
+               date: $("#date").val()
+             };
+             
+             insertGame(game, true);
+             
+             games.push(game);
+             window.localStorage['games'] = JSON.stringify(games);
+             
+           
+        });
+    });
+    
+    function insertGame(g, bShowAlert){
+         var li = $("<li class='list-group-item'>");
+            var datespan = $("<span class='date'>").text(g.date + " ");
+            var titlespan = $("<span class='title'>").text(g.title + " ");
+            var descspan = $("<span class='desc'>").text(g.description).addClass("hidden");
             var deletespan = $("<span class='delete glyphicon glyphicon-trash'>").html("&nbsp;&nbsp;").click(function(){
                 $(this).parent().remove();
                 });
@@ -26,12 +51,19 @@
             
             $("#game").val("");
             $("#description").val("");
-            $("#panel").prepend('<div class="alert alert-success alert-dismissable" id="divalert"><button class="close" data-dismiss="alert">&times;</button>Wedstrijd is toegevoegd!</div>');
-            $(".alert").delay(5000).fadeOut(200);
-        });
-    });
+            if (bShowAlert) {
+              $("#panel").prepend('<div class="alert alert-success alert-dismissable" id="divalert"><button class="close" data-dismiss="alert">&times;</button>Wedstrijd is toegevoegd!</div>');
+              $(".alert").delay(5000).fadeOut(200);
+            }
+    }
     
     function clickLI(){
-        var detailspanel = "<h2>" + $(".title",this).text() + "</h2><p>" + $(".desc", this).text() + "</p>";
+        var title = $(".title",this).text().trim();
+        var detailspanel = "<h2>" + title + "</h2><p>" + $(".desc", this).text() + "</p>";
+        var videopanel = "<h1>" + title + "</h1><video autoplay controls style='width:100%;padding:10px;'><source src='./"+ title + ".mp4'></source></video>";
+        
+        
          $("#details").html(detailspanel);
+         $("#video").html(videopanel);
+
     }
